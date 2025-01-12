f0a6bab52989efe50c38ec5bea986c66c83fc810
evgenia.makarevich <evgenia.makarevich@dizzain.com>
2025-01-12 22:36:45 +0100

    New commit

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,8 @@ module.exports = defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: process.env.CI ? true : false,
+    viewport: { width: 1920, height: 1080},
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
